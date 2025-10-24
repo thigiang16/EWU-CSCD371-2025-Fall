0cfd1fee6ea4ceb246aea5ecd7326e8ac7177d56
Ashley Giang <tgiang1@ewu.edu>
2025-10-24 00:10:59 -0700

    Define Node class

---
+++ GenericsHomework/GenericsHomework/Node.cs
@@ -0,0 +1,19 @@
+﻿namespace GenericsHomework
+{
+    public class Node<T>
+    {
+        public T Value { get; }
+        public Node<T> Next { get; private set; }
+
+        public Node(T value) 
+        {
+            Value = value;
+            Next = this;
+        }
+
+        public override string ToString()
+        {
+            return Value?.ToString() ?? "null";
+        }
+    }
+}
