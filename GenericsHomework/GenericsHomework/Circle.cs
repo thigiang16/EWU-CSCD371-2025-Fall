5aadc813000c3b39630fe52a6d33b148c4f524b7
Ashley Giang <tgiang1@ewu.edu>
2025-10-26 12:27:32 -0700

    Edit comments

--- GenericsHomework/GenericsHomework/Circle.cs
+++ GenericsHomework/GenericsHomework/Circle.cs
@@ -5,12 +5,23 @@
     /// </summary>
     public class Circle<T> where T : class
     {
-        private readonly HashSet<T> _items = new(); //HashSet<T> automatically prevents duplicate items
-        public string Name { get; } //Read-only that stores the name of the circle
-        public Circle(string name) => Name = name; //Initializes a new Circle with a given name
-        public void Add(T item) => _items.Add(item); //Adds an item to the circle
-        public bool Contains(T item) => _items.Contains(item); //Checks whether the given items exists in the circle
-        public IEnumerable<T> GetItems() => _items; //Returns all items in the circle (read-only)
+        //HashSet<T> automatically prevents duplicate items
+        private readonly HashSet<T> _items = new();
+
+        //Read-only that stores the name of the circle
+        public string Name { get; }
+
+        //Initializes a new Circle with a given name
+        public Circle(string name) => Name = name;
+
+        //Adds an item to the circle
+        public void Add(T item) => _items.Add(item);
+
+        //Checks whether the given items exists in the circle
+        public bool Contains(T item) => _items.Contains(item);
+
+        //Returns all items in the circle (read-only)
+        public IEnumerable<T> GetItems() => _items;
         public override string ToString() => $"{Name}: {string.Join(", ", _items)}";
     
     }
--- GenericsHomework/GenericsHomework/VennDiagram.cs
+++ GenericsHomework/GenericsHomework/VennDiagram.cs
@@ -1,5 +1,8 @@
 ﻿namespace GenericsHomework
 {
+    /// <summary>
+    /// Represents a Venn diagram composed of multiple circles
+    /// </summary>
     public class VennDiagram<T> where T : class
     {
         
