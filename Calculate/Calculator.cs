0c9b366ba0d181386660864fa4d41b6f27807a8a
Ashley Giang <tgiang1@ewu.edu>
2025-11-01 16:57:36 -0700

    Define Calculator class

---
+++ Calculate/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculate;
+
+public class Calculator
+{
+    
+    public static double Add(double a, double b) => a + b;
+    public static double Subtract(double a, double b) => a - b;
+    public static double Multiple(double  a, double b ) => a * b;
+    public static double Divide(double a, double b)
+    {
+        if (b == 0) throw new DivideByZeroException("Cannot divide by zero");
+        return a / b;
+    }
+}
