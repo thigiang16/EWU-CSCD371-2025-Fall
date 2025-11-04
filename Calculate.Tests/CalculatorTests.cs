9620708ab7502bd3faf80997a38d0d249dbcc1eb
Ashley Giang <tgiang1@ewu.edu>
2025-11-03 18:12:20 -0800

    fix tests

--- Calculate.Tests/CalculatorTests.cs
+++ Calculate.Tests/CalculatorTests.cs
@@ -6,8 +6,7 @@ public class CalculatorTests
     [TestMethod]
     public void TryCalculate_Addition_ReturnsExpectedResult()
     {
-        Calculator calc = new();
-        bool ok = calc.TryCalculate("3 + 4", out double result);
+        bool ok = Calculator.TryCalculate("3 + 4", out double result);
         Assert.IsTrue(ok);
         Assert.AreEqual<double>(7, result);
     }
@@ -15,8 +14,7 @@ public class CalculatorTests
     [TestMethod]
     public void TryCalculate_Subtraction_ReturnsExpectedResult()
     {
-        Calculator calc = new();
-        bool ok = calc.TryCalculate("10 - 7", out double result);
+        bool ok = Calculator.TryCalculate("10 - 7", out double result);
         Assert.IsTrue(ok);
         Assert.AreEqual<double>(3, result);
     }
@@ -24,8 +22,7 @@ public class CalculatorTests
     [TestMethod]
     public void TryCalculate_Multiplication_ReturnsExpectedResult()
     {
-        Calculator calc = new();
-        bool ok = calc.TryCalculate("2 * 7", out double result);
+        bool ok = Calculator.TryCalculate("2 * 7", out double result);
         Assert.IsTrue(ok);
         Assert.AreEqual<double>(14, result);
     }
@@ -33,8 +30,7 @@ public class CalculatorTests
     [TestMethod]
     public void TryCalculate_Division_ReturnsExpectedResult()
     {
-        Calculator calc = new();
-        bool ok = calc.TryCalculate("10 / 5", out double result);
+        bool ok = Calculator.TryCalculate("10 / 5", out double result);
         Assert.IsTrue(ok);
         Assert.AreEqual<double>(2, result);
     }
@@ -42,24 +38,21 @@ public class CalculatorTests
     [TestMethod]
     public void TryCalculate_NoSpacesAroundOperator_ReturnsFalse()
     {
-        Calculator calc = new();
-        bool temp = calc.TryCalculate("3+4", out double result);
+        bool temp = Calculator.TryCalculate("3+4", out double result);
         Assert.IsFalse(temp);
     }
 
     [TestMethod]
     public void TryCalculate_NonIntegerInput_ReturnsFalse()
     {
-        Calculator calc = new();
-        bool temp = calc.TryCalculate("3.5 + 2", out double result);
+        bool temp = Calculator.TryCalculate("3.5 + 2", out double result);
         Assert.IsFalse(temp);
     }
 
     [TestMethod]
     public void TryCalculate_DivideByZero_ReturnsFalse()
     {
-        Calculator calc = new();
-        bool temp = calc.TryCalculate("10 / 0", out double result);
+        bool temp = Calculator.TryCalculate("10 / 0", out double result);
         Assert.IsFalse(temp);
     }
 
