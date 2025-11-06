e47014c6caea1e0824c1f27f18f818e3ff2832ee
Ashley Giang <tgiang1@ewu.edu>
2025-11-05 21:40:20 -0800

    add more tests

--- Calculate.Tests/ProgramTests.cs
+++ Calculate.Tests/ProgramTests.cs
@@ -1,15 +1,79 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Calculate;
+﻿using Calculate;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Calculate.Tests;
 
 [TestClass]
 public class ProgramTests
 {
+    private static readonly object ConsoleLock = new();
+    /// <summary>
+    /// Helper method to run Program.Main with specified input and capture output.
+    /// </summary>
+    private static string RunProgramWithInput(string input)
+    {
+        lock (ConsoleLock)
+        {
+            StringReader reader = new StringReader(input);
+            StringWriter writer = new StringWriter();
+            TextReader originalIn = Console.In;
+            TextWriter originalOut = Console.Out;
+
+            try
+            {
+                Console.SetIn(reader);
+                Console.SetOut(writer);
+
+                Program.Main();
+
+                string output = writer.ToString();
+                return output;
+            }
+            finally
+            {
+                Console.SetIn(originalIn);
+                Console.SetOut(originalOut);
+            }
+        }
+    }
 
     [TestMethod]
-    public void WriteLine_WorkAsExpected()
+    public void Main_ValidIntegerCalculation_PrintsResult()
+    {
+        string input = "3 + 3";
+        string output = RunProgramWithInput(input);
+        StringAssert.Contains(output, "The result is: 6");
+    }
+
+    [TestMethod]
+    public void Main_ValidDoubleCalculation_PrintsResult()
+    {
+        string input = "7.5 + 2.5";
+        string output = RunProgramWithInput(input);
+        StringAssert.Contains(output, "The result is: 10");
+    }
+
+    [TestMethod]
+    public void Main_InvalidCalculation_OutputsErrorMessage()
+    {
+        string input = "a + 3";
+        string output = RunProgramWithInput(input);
+        StringAssert.Contains(output, "Something went wrong with the calculation");
+    }
+
+    [TestMethod]
+    public void Main_NoInput_OutputsNoInputMessage()
+    {
+        string input = string.Empty;
+        string output = RunProgramWithInput(input);
+        StringAssert.Contains(output, "There wasn't any input");
+    }
+
+    [TestMethod]
+    public void WriteLine_OutputsExpectedText_WhenDelegateIsSet()
     {
         string output = string.Empty;
         var program = new Program
@@ -24,7 +88,7 @@ public class ProgramTests
     }
 
     [TestMethod]
-    public void ReadLine_WorkAsExpected()
+    public void ReadLine_ReturnsExpectedInput_WhenDelegateIsSet()
     {
         string input = "Hello";
         var program = new Program
@@ -36,4 +100,10 @@ public class ProgramTests
 
         Assert.AreEqual<string>(input, receivedInput);
     }
+
+    
+
+
+
+
 }
